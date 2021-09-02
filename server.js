fd7cf73d8b64772d775f6f6c0a08339c030d2793
Airit Simsel <airitkarm@gmail.com>
2021-09-02 17:33:40 +0300

    superheroes/supervillains

---
+++ server.js
@@ -0,0 +1,8 @@
+var superheroes = require('superheroes');
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
+var supervillains = require('supervillains');
+var mySupervillainName = supervillains.random();
+var mySupervillainName = supervillains.all;
+console.log(mySupervillainName);
